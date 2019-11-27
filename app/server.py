054143f49549f2b72d7e720e5b462cb99292779d
Hit-TokiTobashi <58208408+Hit-TokiTobashi@users.noreply.github.com>
2019-11-27 09:37:42 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1JqU2R6mkgNQ40KSHOmT4uWk3dySqGsUF'
 export_file_name = 'export.pkl'
 
-classes = ['charmander', 'bulbasaur', 'pikachu','squirtle']
+classes = ['charmander','bulbasaur','pikachu','squirtle']
 path = Path(__file__).parent
 
 app = Starlette()
